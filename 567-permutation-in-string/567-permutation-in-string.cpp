7557f556597d3c7fe59c4cfedec03051d3e6effa
Narendra Naidu <narendranaidu9441610114@gmail.com>
2022-02-11 09:03:57 +0530

    Time: 4 ms (95.71%), Space: 7.4 MB (39.39%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if(s1.size() > s2.size())
+            return false;
+        vector<int> s1_freq(26, 0), s2_freq(26, 0);
+        
+        // check if the frequency of s1 is there in s2 with a window size equal to s1
+        int i = 0;
+        for(; i < s1.size(); i++) {
+            // compute the frequency of each char for the first window
+            ++s1_freq[s1[i] - 'a'];
+            ++s2_freq[s2[i] - 'a'];
+        }
+        
+        if(s1_freq == s2_freq)
+            return true;
+        // search the remaining windows
+        for(; i < s2.size(); i++) {
+            // remove the first char of current window and add the current char
+            --s2_freq[s2[i - s1.size()] - 'a'];
+            ++s2_freq[s2[i] - 'a'];
+            if(s1_freq == s2_freq)
+                return true;
+        }
+        return false;
+    }
+};
